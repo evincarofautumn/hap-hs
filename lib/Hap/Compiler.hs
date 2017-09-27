3f4c01a98e5a3a825754030671dd2d4ed7321eaa
Jon Purdy <evincarofautumn@gmail.com>
2017-09-26 18:08:50 -0700

    Sketch implementations of most binary operators.

--- lib/Hap/Compiler.hs
+++ lib/Hap/Compiler.hs
@@ -6,11 +6,15 @@ module Hap.Compiler
 
 import Control.Monad (when)
 import Control.Monad.IO.Class (liftIO)
+import Data.Fixed (mod')
+import Data.List ((\\), intersect, union)
 import Data.Maybe (fromJust)
+import Data.Monoid
 import Hap.Language
 import Hap.Runtime
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import qualified Data.Text as Text
 
 compile :: Program -> Either CompileError (Hap ())
 compile (Program statements)
@@ -118,11 +122,285 @@ compileExpression expression = case expression of
   GroupExpression _pos body -> do
     compiledBody <- compileExpression body
     pure $ compiledBody
-{-
-  UnaryExpression !SourcePos !UnaryOperator !Expression
-  BinaryExpression !SourcePos !BinaryOperator !Expression !Expression
--}
+  UnaryExpression _pos operator operand -> do
+    compiledOperand <- compileExpression operand
+    pure $ do
+      operandValue <- compiledOperand
+      pure $ case operator of
+        -- See note [Each and Every].
+        UnaryEach -> error "TODO: implement unary 'each'"
+        UnaryEvery -> error "TODO: implement unary 'every'"
+        UnaryMinus -> case operandValue of
+          IntegerValue value -> IntegerValue $ -value
+          FloatValue value -> FloatValue $ -value
+          _ -> error "argument of unary '-' not a number"
+        UnaryNot -> case operandValue of
+          BooleanValue value -> BooleanValue $ not value
+          _ -> error "argument of unary 'not' not a boolean"
+        UnaryPlus -> case operandValue of
+          IntegerValue{} -> operandValue
+          FloatValue{} -> operandValue
+          _ -> error "argument of unary '+' not a number"
+  BinaryExpression _pos operator first second -> do
+    compiledFirst <- compileExpression first
+    compiledSecond <- compileExpression second
+    pure $ case operator of
+
+      -- TODO: function * function = compose with multiplication
+      BinaryMultiply -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (IntegerValue a, IntegerValue b) -> pure $ IntegerValue $ a * b
+          (FloatValue a, FloatValue b) -> pure $ FloatValue $ a * b
+          (TextValue a, IntegerValue b)
+            -> pure $ TextValue $ mconcat $ replicate (fromIntegral b) a
+          (IntegerValue a, TextValue b)
+            -> pure $ TextValue $ mconcat $ replicate (fromIntegral a) b
+          (ListValue a, IntegerValue b)
+            -> pure $ ListValue $ mconcat $ replicate (fromIntegral b) a
+          (IntegerValue a, ListValue b)
+            -> pure $ ListValue $ mconcat $ replicate (fromIntegral a) b
+          _ -> error "invalid argument to binary '+'"
+
+      -- TODO: function / function = compose with division
+      BinaryDivide -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (IntegerValue a, IntegerValue b) -> pure $ IntegerValue $ a `div` b
+          (IntegerValue a, FloatValue b) -> pure $ FloatValue $ fromIntegral a / b
+          (FloatValue a, IntegerValue b) -> pure $ FloatValue $ a / fromIntegral b
+          (FloatValue a, FloatValue b) -> pure $ FloatValue $ a / b
+          _ -> error "argument of binary '/' not a number"
+
+      -- TODO: function mod function = compose with modulus
+      BinaryModulus -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (IntegerValue a, IntegerValue b) -> pure $ IntegerValue $ a `mod` b
+          (IntegerValue a, FloatValue b) -> pure $ FloatValue $ fromIntegral a `mod'` b
+          (FloatValue a, IntegerValue b) -> pure $ FloatValue $ a `mod'` fromIntegral b
+          (FloatValue a, FloatValue b) -> pure $ FloatValue $ a `mod'` b
+          _ -> error "argument of binary 'mod' not a number"
+
+      -- TODO: function + function = compose with addition
+      BinaryAdd -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (IntegerValue a, IntegerValue b) -> pure $ IntegerValue $ a + b
+          (FloatValue a, FloatValue b) -> pure $ FloatValue $ a + b
+          (TextValue a, TextValue b) -> pure $ TextValue $ a <> b
+          (ListValue a, ListValue b) -> pure $ ListValue $ a <> b
+          (SetValue a, SetValue b) -> pure $ SetValue $ a <> b
+          (MapValue a, MapValue b) -> pure $ MapValue $ Map.unionWith (flip const) a b
+          _ -> error "invalid argument to binary '+'"
+
+      -- TODO: function - function = compose with subtraction
+      BinarySubtract -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (IntegerValue a, IntegerValue b) -> pure $ IntegerValue $ a - b
+          (FloatValue a, FloatValue b) -> pure $ FloatValue $ a - b
+          (SetValue a, SetValue b) -> pure $ SetValue $ a `Set.difference` b
+          (MapValue a, MapValue b) -> pure $ MapValue $ a `Map.difference` b
+          (MapValue a, SetValue b) -> pure $ MapValue $ foldr Map.delete a $ Set.toList b
+          _ -> error "invalid argument to binary '-'"
+
+      -- TODO: function < function = compose with less than
+      BinaryLess -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (BooleanValue a, BooleanValue b) -> pure $ BooleanValue $ a < b
+          (IntegerValue a, IntegerValue b) -> pure $ BooleanValue $ a < b
+          (FloatValue a, FloatValue b) -> pure $ BooleanValue $ a < b
+          (TextValue a, TextValue b) -> pure $ BooleanValue $ a < b
+          (ListValue a, ListValue b) -> pure $ BooleanValue $ a < b
+          (MapValue a, MapValue b) -> pure $ BooleanValue $ a `Map.isProperSubmapOf` b
+          (SetValue a, SetValue b) -> pure $ BooleanValue $ a `Set.isProperSubsetOf` b
+          _ -> error "invalid argument to binary '<'"
+
+      BinaryNotLess -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (BooleanValue a, BooleanValue b) -> pure $ BooleanValue $ a >= b
+          (IntegerValue a, IntegerValue b) -> pure $ BooleanValue $ a >= b
+          (FloatValue a, FloatValue b) -> pure $ BooleanValue $ a >= b
+          (TextValue a, TextValue b) -> pure $ BooleanValue $ a >= b
+          (ListValue a, ListValue b) -> pure $ BooleanValue $ a >= b
+          (MapValue a, MapValue b) -> pure $ BooleanValue $ b `Map.isSubmapOf` a
+          (SetValue a, SetValue b) -> pure $ BooleanValue $ b `Set.isSubsetOf` a
+          _ -> error "invalid argument to binary '>='"
+
+      BinaryGreater -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (BooleanValue a, BooleanValue b) -> pure $ BooleanValue $ a > b
+          (IntegerValue a, IntegerValue b) -> pure $ BooleanValue $ a > b
+          (FloatValue a, FloatValue b) -> pure $ BooleanValue $ a > b
+          (TextValue a, TextValue b) -> pure $ BooleanValue $ a > b
+          (ListValue a, ListValue b) -> pure $ BooleanValue $ a > b
+          (MapValue a, MapValue b) -> pure $ BooleanValue $ b `Map.isProperSubmapOf` a
+          (SetValue a, SetValue b) -> pure $ BooleanValue $ b `Set.isProperSubsetOf` a
+          _ -> error "invalid argument to binary '>'"
+
+      BinaryNotGreater -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (BooleanValue a, BooleanValue b) -> pure $ BooleanValue $ a <= b
+          (IntegerValue a, IntegerValue b) -> pure $ BooleanValue $ a <= b
+          (FloatValue a, FloatValue b) -> pure $ BooleanValue $ a <= b
+          (TextValue a, TextValue b) -> pure $ BooleanValue $ a <= b
+          (ListValue a, ListValue b) -> pure $ BooleanValue $ a <= b
+          (MapValue a, MapValue b) -> pure $ BooleanValue $ a `Map.isSubmapOf` b
+          (SetValue a, SetValue b) -> pure $ BooleanValue $ a `Set.isSubsetOf` b
+          _ -> error "invalid argument to binary '<='"
+
+      BinaryEqual -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (BooleanValue a, BooleanValue b) -> pure $ BooleanValue $ a == b
+          (IntegerValue a, IntegerValue b) -> pure $ BooleanValue $ a == b
+          (FloatValue a, FloatValue b) -> pure $ BooleanValue $ a == b
+          (TextValue a, TextValue b) -> pure $ BooleanValue $ a == b
+          (ListValue a, ListValue b) -> pure $ BooleanValue $ a == b
+          (MapValue a, MapValue b) -> pure $ BooleanValue $ a == b
+          (SetValue a, SetValue b) -> pure $ BooleanValue $ a == b
+          (NullValue, NullValue) -> pure $ BooleanValue True
+          (_, NullValue) -> pure $ BooleanValue False
+          (NullValue, _) -> pure $ BooleanValue False
+          _ -> error "invalid argument to binary '='"
+
+      BinaryNotEqual -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (BooleanValue a, BooleanValue b) -> pure $ BooleanValue $ a /= b
+          (IntegerValue a, IntegerValue b) -> pure $ BooleanValue $ a /= b
+          (FloatValue a, FloatValue b) -> pure $ BooleanValue $ a /= b
+          (TextValue a, TextValue b) -> pure $ BooleanValue $ a /= b
+          (ListValue a, ListValue b) -> pure $ BooleanValue $ a /= b
+          (MapValue a, MapValue b) -> pure $ BooleanValue $ a /= b
+          (SetValue a, SetValue b) -> pure $ BooleanValue $ a /= b
+          (NullValue, NullValue) -> pure $ BooleanValue False
+          (_, NullValue) -> pure $ BooleanValue True
+          (NullValue, _) -> pure $ BooleanValue True
+          _ -> error "invalid argument to binary '<>'"
+
+      BinaryElement -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (a, SetValue b) -> pure $ BooleanValue $ a `Set.member` b
+          (a, ListValue b) -> pure $ BooleanValue $ a `elem` b
+          (a, MapValue b) -> pure $ BooleanValue $ a `Map.member` b
+          (TextValue a, TextValue b) -> pure $ BooleanValue $ a `Text.isInfixOf` b
+          _ -> error "invalid argument to binary 'is in'"
+
+      BinaryNotElement -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (a, SetValue b) -> pure $ BooleanValue $ a `Set.notMember` b
+          (a, ListValue b) -> pure $ BooleanValue $ a `notElem` b
+          (a, MapValue b) -> pure $ BooleanValue $ a `Map.notMember` b
+          (TextValue a, TextValue b) -> pure $ BooleanValue $ not $ a `Text.isInfixOf` b
+          _ -> error "invalid argument to binary 'is not in'"
+
+      -- See note [Short-circuiting Operators].
+      BinaryAnd -> do
+        firstValue <- compiledFirst
+        case firstValue of
+          BooleanValue False -> pure $ BooleanValue False
+          BooleanValue True -> do
+            secondValue <- compiledSecond
+            case secondValue of
+              BooleanValue b -> pure $ BooleanValue b
+              _ -> error "invalid argument to binary 'and'"
+          _ -> do
+            secondValue <- compiledSecond
+            case (firstValue, secondValue) of
+              (ListValue a, ListValue b)
+                -- FIXME: Not sure this notion of list intersection is right.
+                -> pure $ ListValue $ a `intersect` b
+              (MapValue a, MapValue b)
+                -> pure $ MapValue $ a `Map.intersection` b
+              (SetValue a, SetValue b)
+                -> pure $ SetValue $ a `Set.intersection` b
+              _ -> error "invalid argument to binary 'and'"
+
+      -- See note [Short-circuiting Operators].
+      BinaryOr -> do
+        firstValue <- compiledFirst
+        case firstValue of
+          BooleanValue True -> pure $ BooleanValue True
+          BooleanValue False -> do
+            secondValue <- compiledSecond
+            case secondValue of
+              BooleanValue b -> pure $ BooleanValue b
+              _ -> error "invalid argument to binary 'or'"
+          _ -> do
+            secondValue <- compiledSecond
+            case (firstValue, secondValue) of
+              -- FIXME: Should this be (<>)?
+              (ListValue a, ListValue b) -> pure $ ListValue $ a `union` b
+              (MapValue a, MapValue b) -> pure $ MapValue $ Map.unionWith (flip const) a b
+              (SetValue a, SetValue b) -> pure $ SetValue $ a `Set.union` b
+              _ -> error "invalid argument to binary 'or'"
+
+      BinaryXor -> do
+        firstValue <- compiledFirst
+        secondValue <- compiledSecond
+        case (firstValue, secondValue) of
+          (BooleanValue a, BooleanValue b) -> pure $ BooleanValue $ a /= b
+          -- FIXME: Not sure this notion of list symmetric difference is right.
+          (ListValue a, ListValue b) -> pure $ ListValue
+            $ (a <> b) \\ (a `intersect` b)
+          (MapValue a, MapValue b) -> pure $ MapValue
+            $ Map.unionWith (flip const)
+              (a `Map.difference` b)
+              (b `Map.difference` a)
+          (SetValue a, SetValue b) -> pure $ SetValue
+            $ (a `Set.difference` b) `Set.union` (b `Set.difference` a)
+          _ -> error "invalid argument to binary 'xor'"
+
+      -- See note [Short-circuiting Operators].
+      BinaryImplies -> do
+        firstValue <- compiledFirst
+        case firstValue of
+          BooleanValue False -> pure $ BooleanValue True
+          BooleanValue True -> do
+            secondValue <- compiledSecond
+            case secondValue of
+              BooleanValue b -> pure $ BooleanValue b
+              _ -> error "invalid argument to binary 'implies'"
+          _ -> error "invalid argument to binary 'implies'"
+
+      -- set cell
+      BinaryAssign -> error "TODO: implement binary '<-'"
+
   _ -> Left $ "TODO: compile expression: " ++ show expression
+
+-- Note [Short-circuiting Operators]:
+--
+-- It may not be obvious that it's valid to have short-circuiting logical
+-- operators in Hap, but it is correct.
+--
+-- In the expression 'a or b', 'a' is always evaluated. If 'a' is true, then 'b'
+-- is not evaluated, so 'or' doesn't record a dependency on 'b'. However,
+-- whether 'b' is true or false doesn't affect the result of the 'or' expression
+-- as long as 'a' is true. And if 'a' changes from true to false, then the 'or'
+-- expression will be re-evaluated, thereby evaluating 'b' and recording the new
+-- dependency. A similar argument can be made for 'a and b' and 'a implies b'
+-- when 'a' is initially false.
 
 -- Add an action to be run the first time a condition becomes true, after which
 -- it's removed. If true initially, the action is run immediately and the
